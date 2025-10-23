17a80f31590244235f2dd788e8de922b62aa1642
Gustavo Dias <guguiodias@gmail.com>
2025-10-23 15:28:54 -0300

    Início do trabalho de AEDS2
    Trabalho da faculdade

---
+++ Gerador de dados.py
@@ -0,0 +1,28 @@
+from faker import *
+import random
+
+fake = Faker('pt-br')
+arquivo = "dados.dat"
+areas = ["Engenharia", "Ciência", "Gestão", "Tecnologia", "Design", "Administração", "Saúde"]
+especialidades = ["Software", "Dados", "Sistemas", "Ambiental", "Marketing", "Financeira", "Industrial"]
+
+def gerar_curso():
+    return f"{random.choice(areas)} de {random.choice(especialidades)}"
+
+n = int(input('Digite quantos registros você quer gerar: '))
+
+with open(arquivo, "wb") as f:
+    for _ in range(n):
+        matricula = fake.numerify(text='#########')
+        nome = fake.name()[:50]
+        cpf = fake.cpf()
+        curso = gerar_curso()[:30]
+        mae = fake.name_female()[:30]
+        pai = fake.name_male()[:30]
+        ano = fake.year()
+        ca = fake.numerify(text='#.##')
+
+        linha = f"{matricula};{nome};{cpf};{curso};{mae};{pai};{ano};{ca}\n"
+        f.write(linha.encode('utf-8'))
+
+print(f"{n} registros foram salvos em '{arquivo}'.")
